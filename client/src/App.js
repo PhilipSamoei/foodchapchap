60b7984b30815b2f4b694ab405b5a85412d3b565
simonkhartoum <simonkhartoum@gmail.com>
2023-07-27 17:35:17 +0300

    update App

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import ContactUs from './components/ContactUs';
+// import Restaurant from '/components/Restaurant';
+// import Category from '/components/Category';
+
+
 
 function App() {
   return (
-    <div>
-    <HomePage />
-    </div>
+    <div className="App">
+      <BrowserRouter>
 
-  );
+      <NavBar/>
+      <Home/>
+
+      <Routes>
+         <Route
+         path='/' element={<Home/>}/>
+          {/* <Route
+         path='/Restaurant'
+         element={
+          <Restaurant/>
+         }/>
+
+        <Route
+         path='/Category'
+         element={
+          <Category/>
+         }/> */}
+          <Route path='/ContactUs'element={<ContactUs/>}/>
+
+      </Routes>
+
+      </BrowserRouter>
+    </div>
+    );
 }
 
 export default App;
