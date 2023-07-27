8254611cae1d4253717782ad665e864275ed145d
Chris <chris.kamau@student.moringaschool.com>
2023-07-27 13:03:21 +0300

    Add models, controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :reviews, only: [:index, :show, :create, :update,:destroy]
+  resources :orders, only: [:index, :show, :create, :update,:destroy]
+  resources :users, only: [:index, :show, :create, :update,:destroy]
+  resources :admins
+  resources :beverages, only: [:index, :show, :create, :update,:destroy]
+  resources :dishes, only: [:index, :show, :create, :update,:destroy]
+  resources :restaurants, only: [:index, :show, :create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
