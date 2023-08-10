1ffe4d32a463e163b76881c94b8e8bbb04d8df06
Philip Samoei <samphilkerich04@gmail.com>
2023-08-10 14:34:40 +0300

    mpesa api not finished yet

--- config/application.rb
+++ config/application.rb
@@ -37,13 +37,18 @@ module Foodchapchap
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end if File.exists?(env_file)
+    # end
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+      ENV[key.to_s] = value
       end if File.exists?(env_file)
-    end
-
+      end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
@@ -51,3 +56,4 @@ module Foodchapchap
     config.api_only = true
   end
 end
+
--- db/migrate/20230727090437_add_image_to_restaurant.rb
+++
@@ -1,5 +0,0 @@
-class AddImageToRestaurant < ActiveRecord::Migration[7.0]
-  def change
-    add_column :restaurants, :image, :string
-  end
-end
