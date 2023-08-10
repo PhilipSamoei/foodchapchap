886f94a407c67ecc8db96bd6746b5a3357fd120a
Chris <chris.kamau@student.moringaschool.com>
2023-08-10 20:36:32 +0300

    add quantity to cart

--- client/src/pages/CartItems.js
+++ client/src/pages/CartItems.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import '../css/CartStyling.css';
 
-function CartItems({ item, onRemove }) {
+function CartItems({ item, onRemove, onQuantityChange }) {
   return (
     <div className='cart-item'>
       <div className='cart-header'>
         <h2 className='card-title'>
-          {/* {item.restaurant?.name?.charAt(0).toUpperCase() +
-            (item.restaurant?.name?.slice(1).toLowerCase() || '')} -{' '} */}
           {item.name?.charAt(0).toUpperCase() + (item.name?.slice(1).toLowerCase() || '')}
         </h2>
         <div className='cart-category'>
@@ -15,6 +13,29 @@ function CartItems({ item, onRemove }) {
         </div>
         <div className='cart-price'>
           <p>KSH {item.price}.00</p>
+        </div>
+        <div className='cart-quantity'>
+          <button
+            className={`quantity-btn quantity-decrease-btn`}
+            onClick={() =>
+              onQuantityChange(item.id, Math.max(item.quantity - 1, 1)) // Ensure least quantity is 1
+            }
+          >
+            -
+          </button>
+          <input
+            type='number'
+            value={item.quantity}
+            onChange={(e) => onQuantityChange(item.id, parseInt(e.target.value))}
+            className='quantity-input'
+            min='1' // Ensure least quantity is 1
+          />
+          <button
+            className={`quantity-btn quantity-increase-btn`}
+            onClick={() => onQuantityChange(item.id, item.quantity + 1)}
+          >
+            +
+          </button>
         </div>
         <div className='cart-actions'>
           <button onClick={() => onRemove(item.id)} className='rmv-btn'>
